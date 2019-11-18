4d94e1c57d45689ee06bf22e97e602068e406cd0
RinBui <hai.thanhbui@2clicksolutions.com>
2019-11-18 12:40:22 +0700

    update constants

---
+++ src/constants/breakpoints.js
@@ -0,0 +1,29 @@
+const temp = 0.02;
+
+const minXs = 375;
+const maxXs = minXs - temp;
+const minSm = 576;
+const maxSm = minSm - temp;
+const minMd = 768;
+const maxMd = minMd - temp;
+const minLg = 992;
+const maxLg = minLg - temp;
+const minXl = 1200;
+const maxXl = minXl - temp;
+const minXXl = 1400;
+const maxXXl = minXXl - temp;
+
+export default {
+  minXs,
+  maxXs,
+  minSm,
+  maxSm,
+  minMd,
+  maxMd,
+  minLg,
+  maxLg,
+  minXl,
+  maxXl,
+  minXXl,
+  maxXXl,
+};
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,3 @@
-export { default as COLORS } from './colors';
 export { default as THEME } from './theme';
+export { default as ROUTES } from './routes';
+export { default as BREAKPOINTS } from './breakpoints';
